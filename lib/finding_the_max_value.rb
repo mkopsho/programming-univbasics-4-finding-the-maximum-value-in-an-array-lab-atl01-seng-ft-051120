7062fe0564a583a126ab2036fff9605477738c1f
Michael Kopsho <michael.kopsho@gmail.com>
2020-04-09 17:51:08 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,4 +1,12 @@
+# This is specifically against instructions
+# def find_max_value(array)
+#   array.sort!
+#   array[-1]
+# end
+
 def find_max_value(array)
-  array.sort!
-  array[-1]
+  count = 0
+  while count < array.length do
+    array[count]
+  end
 end
